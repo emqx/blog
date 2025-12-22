12b8a95e7a636a3b4917db203f53fe66ed6e8ec2
Ming <i@codming.com>
2025-12-22 11:00:15 +0800

    Fix ci (#1674)
    Signed-off-by: Swilder-M <i@codming.com>

--- .ci/sync_blog.py
+++ .ci/sync_blog.py
@@ -7,24 +7,21 @@ base_path = sys.argv[1]
 
 if __name__ == '__main__':
     langs = ['zh', 'en', 'ja']
-    for site in ['com', 'hstream']:
-        for lang in langs:
-            if lang == 'ja' and site == 'hstream':
-                continue
-            api = f'https://www.emqx.com/api/v1/blog?_sort=updateAt&_limit=1000&site={site}'
-            blog_records = requests.get(url=api, headers={'Content-Language': lang}).json()
-            if not blog_records['success']:
-                print(f'Failed to get blog records: {blog_records}')
-                exit(1)
-            blog_records = blog_records['data']['items']
-            for blog_record in blog_records:
-                blog_lang = blog_record['language']
-                blog_date = '' .join(blog_record['createAt'].split('-')[:2]).replace('-', '')
-                blog_title_url = blog_record['titleUrl']
-                blog_content = blog_record['contents']
-                if not os.path.exists(os.path.join(base_path, blog_lang, blog_date)):
-                    os.makedirs(os.path.join(base_path, blog_lang, blog_date))
+    for lang in langs:
+        api = f'https://www.emqx.com/api/v1/blog?_sort=updateAt&_limit=1000'
+        blog_records = requests.get(url=api, headers={'Content-Language': lang}).json()
+        if not blog_records['success']:
+            print(f'Failed to get blog records: {blog_records}')
+            exit(1)
+        blog_records = blog_records['data']['items']
+        for blog_record in blog_records:
+            blog_lang = blog_record['language']
+            blog_date = '' .join(blog_record['createAt'].split('-')[:2]).replace('-', '')
+            blog_title_url = blog_record['titleUrl']
+            blog_content = blog_record['contents']
+            if not os.path.exists(os.path.join(base_path, blog_lang, blog_date)):
+                os.makedirs(os.path.join(base_path, blog_lang, blog_date))
 
-                f = open(os.path.join(base_path, blog_lang, blog_date, blog_title_url + '.md'), 'w')
-                f.write(blog_content.strip() + '\n')
-                print(f'Update {blog_lang}/{blog_date}/{blog_title_url}')
+            f = open(os.path.join(base_path, blog_lang, blog_date, blog_title_url + '.md'), 'w')
+            f.write(blog_content.strip() + '\n')
+            print(f'Update {blog_lang}/{blog_date}/{blog_title_url}')
